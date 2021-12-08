5ab88ada8205bbc0818431b5c954ec938ef16270
Sasha Jeltuhin <sjeltuhin@gmail.com>
2021-12-07 22:49:15 -0500

    added diagLine and strafeLine strategies

--- src/entity/MoveStrategy.java
+++ src/entity/MoveStrategy.java
@@ -3,10 +3,12 @@ package entity;
 public enum MoveStrategy {
     Curve,
     Diag,
+    DiagLine,
     Spin,
     SpinNCurve,
     SpinNStraight,
     Strafe,
+    StrafeLine,
     StrafeRelative,
     Straight,
     StraightRelative,
